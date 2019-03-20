beb4d52d68d88079b9216f949d41e98230489684
Sarah Doherty Granoff <sdgranoff@gmail.com>
2019-03-20 18:56:29 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,12 @@
 # Write your code here.
+
+katz_deli = []
+
+
+
+def take_a_number(katz_deli, name)
+  katz_deli.push(name)
+  puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
+end
+
+def now_serving(katz_deli)
