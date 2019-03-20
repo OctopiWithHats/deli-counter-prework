a50f688a4fa8f0eae02560553940fc5f7d39e5eb
Sarah Doherty Granoff <sdgranoff@gmail.com>
2019-03-20 18:59:32 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -9,9 +9,9 @@ end
 
 def line (katz_deli)
   if katz_deli.length == 0
-    puts "The line is currently empty."
+    message = "The line is currently empty."
   else
-    puts "The line is currently:"
+    message = "The line is currently:"
     
     katz_deli.each_with_index do |value, index|
-      
+      message += 
